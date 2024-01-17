c485243852500741acc880b3bfc91c9ef513b8c8
1Cezzo <emiljohnzen@gmail.com>
2024-01-17 17:14:09 +0100

    initial commit

---
+++ vue-test/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  components: {
+    Calculator,
+  },
+};
+</script>
