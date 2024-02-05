1ec865dd44ab1acd965bbe83eb036a78b756a58d
1Cezzo <emiljohnzen@gmail.com>
2024-02-05 22:09:22 +0100

    removed comments from test class

--- src/components/__tests__/Calculator.test.ts
+++ src/components/__tests__/Calculator.test.ts
@@ -19,34 +19,29 @@ test('Calculator: Test handleButtonClick method', async () => {
   calculator.handleButtonClick('5');
   assert.equal(calculator.expression, '78+5');
 
-  // Test equals
   calculator.handleButtonClick('=');
-  assert.equal(calculator.expression, ''); // Assuming the result is 83
+  assert.equal(calculator.expression, '');
   assert.equal(calculator.answers[calculator.answers.length - 1], '78+5 = 83');
   assert.equal(calculator.answers.length, 1);
 
-  // Test clear
   calculator.handleButtonClick('C');
   assert.equal(calculator.expression, '');
 
-  // Test deletion
   calculator.handleButtonClick('1');
   calculator.handleButtonClick('2');
   calculator.handleButtonClick('DEL');
   assert.equal(calculator.expression, '1');
 
-  // Test division by zero
   calculator.handleButtonClick('/');
   calculator.handleButtonClick('0');
   calculator.handleButtonClick('=');
   assert.equal(calculator.expression, '');
   assert.equal(calculator.answers[calculator.answers.length - 1], 'undefined');
 
-  // Test using 'ans'
   calculator.handleButtonClick('7');
   calculator.handleButtonClick('+');
   calculator.handleButtonClick('ans');
-  assert.equal(calculator.expression, '7+undefined'); // Assuming the previous result was 83
+  assert.equal(calculator.expression, '7+undefined');
 
   wrapper.unmount();
 
